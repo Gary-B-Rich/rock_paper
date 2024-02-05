b4ea1627ffa685a80b67a19706a3dff72bd0a900
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-02-05 11:32:07 -0500

    Add buttons and text

--- rp_rework/script.js
+++ rp_rework/script.js
@@ -1,1 +1,25 @@
 // script.js for rp_rework
+
+// set variables
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+
+// initialize buttons
+button1.onclick = chooseRock;
+button2.onclick = choosePaper;
+button3.onclick = chooseScissors;
+
+// define functions
+function chooseRock() {
+
+}
+
+function choosePaper() {
+
+}
+
+function chooseScissors() {
+
+}
